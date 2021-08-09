d9960e4f83a44a0a73be9d9de781a2ee46e424c3
xyzrlee <cnrickylee@gmail.com>
2021-08-09 23:22:00 +0800

    try fix

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker-compose -f docker-compose-build.yaml build $@
 
 TAGNAME=`curl https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | jq -r .tag_name`
-echo "TAGNAME=${TAGNAME}" >>${GITHUB_ENV}
+echo "${TAGNAME}" >>TAGNAME
