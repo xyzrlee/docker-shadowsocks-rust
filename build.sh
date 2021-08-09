fb80a8e97600a77b718a368fb64347a98633afa4
xyzrlee <cnrickylee@gmail.com>
2021-08-09 23:19:49 +0800

    test version cache

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker-compose -f docker-compose-build.yaml build $@
+
+TAGNAME=`curl https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | jq -r .tag_name`
+echo "TAGNAME=${TAGNAME}" >>${GITHUB_ENV}
